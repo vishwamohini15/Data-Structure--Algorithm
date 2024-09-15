16ad5f65a5ad7de7b58d4f606ada6222218fe7f1
vishwamohini15 <vishwamohini0015@gmail.com>
2024-09-15 23:26:58 +0530

    day10

---
+++ day9/linearsearch.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int linearserch(int arr[], int sz, int trgt){
+     
+
+}
+
+int main(){
+     int arr[]={2,8,9,4,6};
+     int size=5;
+     int target=9;
+
+}
