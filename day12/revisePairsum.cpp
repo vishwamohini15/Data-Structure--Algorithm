33ea73be1e67ae481e6cf8a6b4dc2fb9b9f540f7
vishwamohini15 <vishwamohini0015@gmail.com>
2024-09-23 21:07:05 +0530

    pairsum

---
+++ day12/revisePairsum.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+vector<int> pairsum(vector<int>nums, int target){
+     vector <int> ans;
+
+     for (int i = 0; i < nums.size(); i++)
+     {
+          for (int j =i+1; j <nums.size(); j++)
+          {
+            if (nums[i] + nums[j]== target)
+            {
+               ans.push_back(i);
+               ans.push_back(j);
+               return ans;
+
+            }
+            
+          }
+          return ans;
+     }
+     
+
+
+}
+
+int main(){
+     vector <int> nums {2,7,11,15};
+     int target=13;
+
+vector<int> ans=pairsum(nums, target);
+cout<<ans[0]<<","<<ans[1]<<" ";
+}
--- day12/secondmethodpairsum.cpp
+++ day12/secondmethodpairsum.cpp
@@ -1,38 +1,39 @@
 #include <iostream>
-#include <vector>
+#include<vector>
 using namespace std;
 
-vector<int> pairsum(vector<int> nums, int target){
+vector<int> pairsum(vector<int>nums, int target){
      vector<int> ans;
      int n=nums.size();
      int i=0;
      int j=n-1;
 
-     while (i<j)
+     while (i < j)
      {
-          int pairsum=nums[i] + nums[j];
+          int pairsum=nums[i]+nums[j];
           if (pairsum>target)
           {
                j--;
-          }else if (pairsum < target)
+          }else if (pairsum<target)
           {
-               i++;
+              i--;
           }else
           {
                ans.push_back(i);
                ans.push_back(j);
-                    return ans;
+               return ans;
+
           }
-          
      }
+     
           return ans;
 }
 
 int main(){
-     vector<int> nums={2,7,11,15};
+     vector<int>nums {2,7,11,15};
      int target=22;
 
-     vector<int> ans=pairsum(nums, target);
+   vector<int> ans=pairsum(nums, target);
      cout<<ans[0]<<" , "<<ans[1]<<endl;
      return 0;
 }
