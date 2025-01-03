1b4634049d28d86eb275c66cbc6c26d3830e06a7
vishwamohini15 <vishwamohini0015@gmail.com>
2025-01-03 23:27:45 +0530

    matrix

--- day35/SearchIn2Dmatrix.cpp
+++ day35/SearchIn2Dmatrix.cpp
@@ -54,6 +54,6 @@ bool SearchInMatrix(vector<vector<int>>& matrix, int target){
 
 int main(){
      vector<vector<int>> matrix{{1,3,5,7},{10,11,16,20},{23,30,34,60}};
-     int target=3;
+     int target=16;
       cout<<SearchInMatrix(matrix,target);
 }
