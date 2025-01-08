338d993c27cd2392845377f96b01e0b648911163
vishwamohini15 <vishwamohini0015@gmail.com>
2025-01-08 23:36:25 +0530

    linkdlist

---
+++ day55/linkdlist.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+class node{
+     public:
+     int data;
+     node* next;
+
+     node(int val){
+          data=val;
+          next=NULL;
+     }
+};
+
+class list{
+     node* head;
+     node* tail;
+
+     public:
+     list(){
+          head=tail=NULL;
+     }
+
+//create a new node...
+     void push_front(int val){
+          node* newnode=new node(val);
+          if (head==NULL)
+          {
+              head=tail=newnode ;
+              return;
+          }
+          
+     }
+};
+
+ int main(){
+     list ll;
+
+return 0;
+ }
