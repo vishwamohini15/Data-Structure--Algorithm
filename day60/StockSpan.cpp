3bad431aacfaa642b194cefd4cddc3037b6c8bad
vishwamohini15 <vishwamohini0015@gmail.com>
2025-02-09 15:02:30 +0530

    Stack, StockSpan

---
+++ day60/StockSpan.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include<vector>
+#include <stack>
+using namespace std;
+
+int main(){
+     //stock price
+     vector<int> price= {100, 80, 60, 70, 60, 75, 85};
+
+     //solution
+     vector<int> ans(price.size(), 0);
+     stack<int> s;
+     for(int i=0; i<price.size(); i++){
+          while (s.size() > 0 && price[s.top()] <= price[i])
+          {
+               s.pop();
+          }
+          
+          if (s.empty())
+          {
+               ans[i]= i+1;
+          }else
+          {
+               ans[i]=i-s.top();
+          }
+          
+          s.push(i);
+     }
+     //print val
+     for(int val:ans){
+          cout<< val<<" ";
+     }
+     cout<<endl;
+}
