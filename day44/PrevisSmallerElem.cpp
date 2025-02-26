359d6a1cf1c35bc557a7417081582c73bd2c31c8
vishwamohini15 <vishwamohini0015@gmail.com>
2025-02-26 21:21:39 +0530

    Prevelement

---
+++ day44/PrevisSmallerElem.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+#include <stack>
+using namespace std;
+
+vector<int> previousSmallerElm(vector<int> arr){
+     vector<int> ans(arr.size(), 0);
+     stack<int> s;
+
+     for (int i =0 ; i < arr.size(); i++)
+     {
+         while (s.size()>0 && s.top()>= arr[i])
+         {
+          s.pop();
+         }
+         if (s.empty())
+         {
+          arr[i]=-1;
+         }else
+         {
+          ans[i]=s.top();
+          
+         }
+         s.push(arr[i]);
+         
+     }
+     return ans;
+}
+
+int main(){
+     vector<int> arr ={3,1,0,8,6};
+
+     vector<int> ans= previousSmallerElm(arr);
+
+     for(int val:ans){
+          cout<<val<<" ";
+     }
+     cout<<endl;
+}
---
+++ day47/Try.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+
+int main(){
+     //stock price
+     vector<int> price={100,80,60,70,60,75,85};
+
+     vector<int> ans;
+     stack<int> s;
+
+     for (int i = 0; i <price.size(); i++)
+     {
+          while (s.size()>0 && price[s.top()]<= price[i])
+          {
+               s.pop();
+          }
+          if (s.empty())
+          {
+               ans[i]=i+1;
+          }else{
+               ans[i]=i-s.top();
+          }
+          s.push(i);
+     }
+       for(int val : ans){
+               cout<<val<<" ";
+          }
+          cout<<endl;
+return 0;
+}
